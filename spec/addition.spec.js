04a2a8c03fe8e83627461cc249079d78e45c4b5c
Johan Bester <johan.bester@yahoo.com>
2021-10-25 19:23:37 -0500

    Updating Testing

---
+++ spec/addition.spec.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const addition = require("./../addition");
+
+describe("addition()", function() {
+  // Arrange
+  let a = 8;
+  let b = 25;
+  let sum = 33;
+
+  //Act
+  let result = addition(a, b);
+
+  //Assert
+  it("it should return the sum of 33", function() {
+    expect(result).toEqual(sum);
+  });
+});
